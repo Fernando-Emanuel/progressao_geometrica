df5ddd5ffdba629a2bf5fe0b6c0ea6184d79e730
Fernando-Emanuel <fernando2004emanuel@gmail.com>
2022-04-06 17:39:51 +0000

    Amém

---
+++ Teste.java
@@ -0,0 +1,15 @@
+public class Teste {
+    public static void main(String[] args) {
+        int num1 = 125;
+        int num2 = 0;
+
+        System.out.println(num2);
+        System.out.println(num1);
+
+        for(int i = 0;i < 9; i++) {
+            num1 = num1 * 5;
+            
+            System.out.println(num1);
+        }
+    }
+}
